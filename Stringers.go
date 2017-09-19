c74cf5893559bd261b698848709f601e5628f0bc
shilfol <yayananoda@gmail.com>
2017-09-19 17:21:52 +0900

    Create Stringers.go

---
+++ Stringers.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+type IPAddr [4]byte
+
+// TODO: Add a "String() string" method to IPAddr.
+type Stringer interface {
+    String() string	
+}
+
+func (addr IPAddr) String() string {
+	return fmt.Sprintf("%d.%d.%d.%d",addr[0],addr[1],addr[2],addr[3])
+}
+
+func main() {
+	hosts := map[string]IPAddr{
+		"loopback":  {127, 0, 0, 1},
+		"googleDNS": {8, 8, 8, 8},
+	}
+	for name, ip := range hosts {
+		fmt.Printf("%v: %v\n", name, ip)
+	}
+}
